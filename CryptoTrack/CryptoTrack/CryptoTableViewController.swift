a8b8a90dc638a7cf7ec73e3b7858a2c98e97d61d
jonathankkizer <jonathankkizer@gmail.com>
2017-10-30 18:15:52 -0500

    Screen now shows top 10 cryptocurrencies
    WOOOO

--- CryptoTrack/CryptoTrack/CryptoTableViewController.swift
+++ CryptoTrack/CryptoTrack/CryptoTableViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class CryptoTableViewController: UITableViewController {
 
+    public var cryptoCurrencies = [Ticker]()
     public var cryptoCurrency1: Ticker?
     public var cryptoCurrency2: Ticker?
     public var cryptoCurrency3: Ticker?
@@ -29,16 +30,27 @@ class CryptoTableViewController: UITableViewController {
             case .success(let tickers):
                 //print(tickers)
                 self.cryptoCurrency1 = tickers[0]
+                self.cryptoCurrencies.append(self.cryptoCurrency1!)
                 self.cryptoCurrency2 = tickers[1]
+                self.cryptoCurrencies.append(self.cryptoCurrency2!)
                 self.cryptoCurrency3 = tickers[2]
+                self.cryptoCurrencies.append(self.cryptoCurrency3!)
                 self.cryptoCurrency4 = tickers[3]
+                self.cryptoCurrencies.append(self.cryptoCurrency4!)
                 self.cryptoCurrency5 = tickers[4]
+                self.cryptoCurrencies.append(self.cryptoCurrency5!)
                 self.cryptoCurrency6 = tickers[5]
+                self.cryptoCurrencies.append(self.cryptoCurrency6!)
                 self.cryptoCurrency7 = tickers[6]
+                self.cryptoCurrencies.append(self.cryptoCurrency7!)
                 self.cryptoCurrency8 = tickers[7]
+                self.cryptoCurrencies.append(self.cryptoCurrency8!)
                 self.cryptoCurrency9 = tickers[8]
+                self.cryptoCurrencies.append(self.cryptoCurrency9!)
                 self.cryptoCurrency10 = tickers[9]
+                self.cryptoCurrencies.append(self.cryptoCurrency10!)
                 self.tableView.reloadData()
+                
                 self.safeToUnwrap = true
                 self.testing()
                 print("Cryptocurrencies Assigned!")
@@ -89,13 +101,16 @@ class CryptoTableViewController: UITableViewController {
     var counter = 0
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cryptoCellid", for: indexPath) as! CryptoTableViewCell
+        print(indexPath.row)
+        
         if safeToUnwrap == false {
-            cell.currencyName?.text = "Loading Data!"
-            cell.currencyPrice?.text = "Loading Data!"
+            cell.currencyName?.text = "🛠🛠🛠"
+            cell.currencyPrice?.text = "🛠🛠🛠"
             return cell
         } else {
-            cell.currencyName?.text = "\(cryptoCurrency1!.id)"
-            cell.currencyPrice?.text = "\(cryptoCurrency1!.priceUSD!)"
+            let currentCurrency = cryptoCurrencies[indexPath.row]
+            cell.currencyName?.text = currentCurrency.name
+            cell.currencyPrice?.text = "$" + "\(currentCurrency.priceUSD!)"
             return cell
         
         }
